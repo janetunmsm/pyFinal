ac33bca8e15db3c08737d7efafe34bc9a27da468
My Name <thesame@email.com>
2025-03-24 17:23:58 -0500

    cambios

---
+++ recurso.js
@@ -0,0 +1,93 @@
+function desplaza() {
+    if (document.getElementById('r1').checked) {
+        if (document.getElementById('r3').checked) {
+            if (document.getElementById('r5').checked) {
+                if (document.getElementById('r7').checked) {
+                    document.getElementById("div1").style.backgroundColor = "#DCDCDC";
+                    document.getElementById("img1").style.marginLeft = "2px";
+                    document.getElementById("img1").style.width = "20%";
+                    document.getElementById("img1").style.height = "30%";
+                    document.getElementById("tImagen").style.fontFamily = "Arial";
+                }
+            }
+        }
+    }
+    if (document.getElementById('r1').checked) {
+        if (document.getElementById('r3').checked) {
+            if (document.getElementById('r6').checked) {
+                if (document.getElementById('r7').checked) {
+                    document.getElementById("div1").style.backgroundColor = "#DCDCDC";
+                    document.getElementById("img1").style.marginLeft = "2px";
+                    document.getElementById("img1").style.width = "28%";
+                    document.getElementById("img1").style.height = "55%";
+                    document.getElementById("tImagen").style.fontFamily = "sans-serif";
+                }
+            }
+        }
+    }
+    if (document.getElementById('r1').checked) {
+        if (document.getElementById('r4').checked) {
+            if (document.getElementById('r5').checked) {
+                if (document.getElementById('r8').checked) {
+                    document.getElementById("div1").style.backgroundColor = "#DCDCDC";
+                    document.getElementById("img1").style.marginLeft = "700px";
+                    document.getElementById("img1").style.width = "20%";
+                    document.getElementById("img1").style.height = "30%";
+                    document.getElementById("tImagen").style.fontFamily = "Segoe";
+
+                }
+            }
+        }
+    }
+
+    if (document.getElementById('r1').checked) {
+        if (document.getElementById('r4').checked) {
+            if (document.getElementById('r6').checked) {
+                document.getElementById("div1").style.backgroundColor = "#DCDCDC";
+                document.getElementById("img1").style.marginLeft = "700px";
+                document.getElementById("img1").style.width = "28%";
+                document.getElementById("img1").style.height = "55%";
+            }
+        }
+    }
+    if (document.getElementById('r2').checked) {
+        if (document.getElementById('r3').checked) {
+            if (document.getElementById('r5').checked) {
+                document.getElementById("div1").style.backgroundColor = "#F0FFFF";
+                document.getElementById("img1").style.marginLeft = "2px";
+                document.getElementById("img1").style.width = "20%";
+                document.getElementById("img1").style.height = "30%";
+            }
+        }
+    }
+    if (document.getElementById('r2').checked) {
+        if (document.getElementById('r3').checked) {
+            if (document.getElementById('r6').checked) {
+                document.getElementById("div1").style.backgroundColor = "#F0FFFF";
+                document.getElementById("img1").style.marginLeft = "2px";
+                document.getElementById("img1").style.width = "28%";
+                document.getElementById("img1").style.height = "55%";
+            }
+        }
+    }
+    if (document.getElementById('r2').checked) {
+        if (document.getElementById('r4').checked) {
+            if (document.getElementById('r5').checked) {
+                document.getElementById("div1").style.backgroundColor = "#F0FFFF";
+                document.getElementById("img1").style.marginLeft = "700px";
+                document.getElementById("img1").style.width = "20%";
+                document.getElementById("img1").style.height = "30%";
+            }
+        }
+    }
+    if (document.getElementById('r2').checked) {
+        if (document.getElementById('r4').checked) {
+            if (document.getElementById('r6').checked) {
+                document.getElementById("div1").style.backgroundColor = "#F0FFFF";
+                document.getElementById("img1").style.marginLeft = "700px";
+                document.getElementById("img1").style.width = "28%";
+                document.getElementById("img1").style.height = "55%";
+            }
+        }
+    }
+}
